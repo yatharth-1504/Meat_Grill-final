1e75e9d3719995119e5a03cbf101cfda48810b1c
Rutwik R Patil <rutwik76@gmail.com>
2024-05-30 02:25:29 +0530

    Social, texts, locations, pic events

--- contact.php
+++ contact.php
@@ -8,7 +8,7 @@
  */
 
 // please change this to your E-Mail address
-$sendTo = "your-email@company.com";
+$sendTo = "info@meatgrill.co.uk";
 
 $action = $_POST['action'];
 if ($action == 'contact') {
